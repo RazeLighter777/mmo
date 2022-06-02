ab32fefa8020765b4e935ec35b65a4a238e9a50b
RazeLighter777 <justin.suess@westpoint.edu>
2022-06-02 09:31:59 -0500

    removed backend leftover

--- backend/context.rs
+++
@@ -1,6 +0,0 @@
-use crate::{raws::RawTree, registry};
-
-pub struct Context<'a> {
-    pub raws: &'a RawTree,
-    pub registry : &'a registry::Registry
-}
--- backend/hashing.rs
+++
@@ -1,5 +0,0 @@
-use const_fnv1a_hash::fnv1a_hash_64;
-
-pub const fn string_hash(input: &str) -> u64 {
-    fnv1a_hash_64(input.as_bytes(), None)
-}
